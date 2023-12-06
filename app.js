ee0a7b0b9c200bae07729cd433f7153431bd67c1
tiharaM <128646241+tiharaM@users.noreply.github.com>
2023-12-06 16:03:10 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-// JavaScript para controlar el carrusel
-const carouselList = document.querySelector('.carousel-list');
-const prevBtn = document.querySelector('#prevBtn');
-const nextBtn = document.querySelector('#nextBtn');
+document.addEventListener('DOMContentLoaded', function () {
+    const carouselList = document.querySelector('.carousel-list');
+    const prevBtn = document.querySelector('#prevBtn');
+    const nextBtn = document.querySelector('#nextBtn');
 
-let currentIndex = 0;
+    let currentIndex = 0;
 
-prevBtn.addEventListener('click', () => {
-    currentIndex = (currentIndex - 1 + carouselList.children.length) % carouselList.children.length;
-    updateCarousel();
+    prevBtn.addEventListener('click', () => {
+        currentIndex = (currentIndex - 1 + carouselList.children.length) % carouselList.children.length;
+        updateCarousel();
+    });
+
+    nextBtn.addEventListener('click', () => {
+        currentIndex = (currentIndex + 1) % carouselList.children.length;
+        updateCarousel();
+    });
+
+    function updateCarousel() {
+        const offset = -currentIndex * 100 + '%';
+        carouselList.style.transform = `translateX(${offset})`;
+    }
 });
-
-nextBtn.addEventListener('click', () => {
-    currentIndex = (currentIndex + 1) % carouselList.children.length;
-    updateCarousel();
-});
-
-function updateCarousel() {
-    const offset = -currentIndex * 100 + '%';
-    carouselList.style.transform = `translateX(${offset})`;
-}
