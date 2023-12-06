5b0b78732342ef13daaedc65f0d01fc52b6d59d5
tiharaM <128646241+tiharaM@users.noreply.github.com>
2023-12-06 15:49:46 -0300

    Update app.js

--- app.js
+++ app.js
@@ -18,3 +18,4 @@ nextBtn.addEventListener('click', () => {
 function updateCarousel() {
     const offset = -currentIndex * 100 + '%';
     carouselList.style.transform = `translateX(${offset})`;
+}
